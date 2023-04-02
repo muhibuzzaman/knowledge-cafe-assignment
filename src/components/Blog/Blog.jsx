97cc02eb4bc0ff5a7e5372656c6b904bbc9573dc
muhib-kagozi <muhib.kagozi@gmail.com>
2023-04-03 02:16:15 +0600

    author name,date,read-time and button are done

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Blog.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBookmark } from '@fortawesome/free-solid-svg-icons'
 
 const Blog = (props) => {
     const { id, cover_img, author_img, title, author_name, release_date, read_time } = props.blog;
@@ -9,7 +11,7 @@ const Blog = (props) => {
             <h3 className='title'>{title}</h3>
 
             <div className='author'>
-                <div>
+                <div className='author-info-container'>
                     <img src={author_img} alt="" />
                     <div className='author-info'>
                         <p className='author-name'>{author_name}</p>
@@ -19,6 +21,7 @@ const Blog = (props) => {
 
                 <div className='read-time'>
                     <p>{read_time} min read</p>
+                    <button><FontAwesomeIcon icon={faBookmark} /></button>
                 </div>
             </div>
         </div>
